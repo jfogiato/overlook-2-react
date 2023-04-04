3e8bde8a811f3c71013cc41804d96859555a63ce
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-04 10:14:48 -0400

    Add: login page form build out and base functionality

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './LoginPage.css';
+import hotelLogo from '../../images/hotel-logo.png';
+import LoginForm from '../LoginForm/LoginForm';
 
-function LoginPage() {
+export default function LoginPage() {
   
   return (
     <section className='login-page'>
-      
+      <img src={hotelLogo} className='login-logo' alt='Grand Budapest Hotel Logo'/>
+      <LoginForm />
     </section>
   );
 }
